f92b87f1ce01533d1d3d3a9ed2093afc81e711b2
Philip Högfeldt <112342813+Philiphogfeldt@users.noreply.github.com>
2023-05-09 21:25:06 +0200

    Layout Start. site css och homepage

--- Migrations/20230509173630_init.Designer.cs
+++ Migrations/20230509173630_init.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend_Task03.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20230509081241_init")]
+    [Migration("20230509173630_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
