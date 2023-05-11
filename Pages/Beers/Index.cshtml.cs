f9f4d6b18107796914104aaaf915b21ad68089b8
Philiphogfeldt <philip.hogfeldteberdal@studerande.plushogskolan.se>
2023-05-11 16:02:12 +0200

    Search Funkar!!!!

--- Pages/Beers/Index.cshtml.cs
+++ Pages/Beers/Index.cshtml.cs
@@ -1,127 +1,162 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend_Task03.Data;
+using Backend_Task03.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using Backend_Task03.Data;
-using Backend_Task03.Models;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using NuGet.Protocol;
 
 namespace Backend_Task03.Pages.Beers
 {
-    public class IndexModel : PageModel
-    {
-        private readonly Backend_Task03.Data.AppDbContext database;
+	public class IndexModel : PageModel
+	{
+		private readonly AppDbContext database;
 
-        public IndexModel(Backend_Task03.Data.AppDbContext context)
-        {
-            database = context;
-        }
+		public IndexModel(AppDbContext context)
+		{
+			database = context;
+		}
 
-        public IList<Beer> Beer { get; set; }
+		public IList<Beer> Beer { get; set; }
 
-        //public string Goesw { get; set; }
+		[BindProperty(SupportsGet = true)]
+		public string FindBeer { get; set; }
 
-        [BindProperty (SupportsGet = true)] 
-        public string Chicken { get; set; } 
-        public string Meat { get; set; }
-        public string Fish { get; set; }
-        public string Vegetarian { get; set; }
-        public string Dessert { get; set; }
+		[BindProperty(SupportsGet = true)]
+		public string[] BeerType { get; set; }
 
-        public async Task OnGetAsync()
-        {
-            Beer = await database.Beers.ToListAsync();
+		[BindProperty(SupportsGet = true)]
+		public string Chicken { get; set; }
+
+		public string Meat { get; set; }
+		public string Fish { get; set; }
+		public string Vegetarian { get; set; }
+		public string Dessert { get; set; }
+
+		public async Task OnGetAsync(string searchInput)
+		{
+			IQueryable<Beer> beers = database.Beers;
+
+			if (!string.IsNullOrEmpty(searchInput))
+			{
+				beers = beers.Where(b => b.Name.Contains(searchInput) || b.EAN13 == searchInput);
+			}
+			if (!string.IsNullOrEmpty(FindBeer))
+			{
+				beers = beers.Where(b => b.Name.Contains(FindBeer));
+			}
+
+			if (BeerType != null && BeerType.Length > 0)
+			{
+				List<string> types = new List<string>();
+				if (BeerType.Contains("Ale"))
+				{
+					types.AddRange(new string[] { "Ale", "Brown Ale", "IPA", "Wheat Ale", "Belgian Ale", "Saison" });
+				}
+				if (BeerType.Contains("Lager"))
+				{
+					types.AddRange(new string[] { "Lager", "Kolsch", "STUFF HERE", "SORTER NÄR SOM NU" });
+				}
+				if (BeerType.Contains("Stout"))
+				{
+					types.AddRange(new string[] { "Stout", "Imperial Stout", "PUT THING HERE", "PUT IT HERE YES" });
+				}
+				beers = beers.Where(b => types.Contains(b.Type));
+			}
+
+			Beer = await beers.ToListAsync();
 
 
-                var beers = await database.Beers.Include(b => b.Reviews).ThenInclude(r => r.FoodCategories).ToListAsync();
+			Beer = await beers.ToListAsync();
 
-                foreach (var beer in beers)
-                {
-                    Dictionary<string, int> categoryCounts = new Dictionary<string, int>();
+			var allBeers = await database.Beers.Include(b => b.Reviews).ThenInclude(r => r.FoodCategories).ToListAsync();
 
-                    // Count the number of times each category appears in the reviews
-                    foreach (var review in beer.Reviews)
-                    {
-                        foreach (var category in review.FoodCategories)
-                        {
-                            if (categoryCounts.ContainsKey(category.Name))
-                            {
-                                categoryCounts[category.Name]++;
-                            }
-                            else
-                            {
-                                categoryCounts[category.Name] = 1;
-                            }
-                        }
-                    }
+			foreach (var beer in allBeers)
+			{
+				Dictionary<string, int> categoryCounts = new Dictionary<string, int>();
 
-                // Find the category/categories with the highest count
-                List<string> mostSelectedCategories = new List<string>();
-                int highestCount = 0;
-                foreach (var kvp in categoryCounts)
-                {
-                    if (kvp.Value > highestCount)
-                    {
-                        mostSelectedCategories.Clear();
-                        mostSelectedCategories.Add(kvp.Key);
-                        highestCount = kvp.Value;
-                    }
-                    else if (kvp.Value == highestCount)
-                    {
-                        mostSelectedCategories.Add(kvp.Key);
+				// Count the number of times each category appears in the reviews
+				foreach (var review in beer.Reviews)
+				{
+					foreach (var category in review.FoodCategories)
+					{
+						if (categoryCounts.ContainsKey(category.Name))
+						{
+							categoryCounts[category.Name]++;
+						}
+						else
+						{
+							categoryCounts[category.Name] = 1;
+						}
+					}
+				}
 
-                        if (kvp.Key == Chicken)
-                        {
-                            
-                        };
-                        
-                    }
-                }
+				// Find the category/categories with the highest count
+				List<string> mostSelectedCategories = new List<string>();
+				int highestCount = 0;
+				foreach (var kvp in categoryCounts)
+				{
+					if (kvp.Value > highestCount)
+					{
+						mostSelectedCategories.Clear();
+						mostSelectedCategories.Add(kvp.Key);
+						highestCount = kvp.Value;
+					}
+					else if (kvp.Value == highestCount)
+					{
+						mostSelectedCategories.Add(kvp.Key);
 
-                // Update the GoesWellWith property
-                beer.GoesWellWith = string.Join(", ", mostSelectedCategories);
-                }
+						if (kvp.Key == Chicken)
+						{
+							// Do something with Chicken
+						}
+					}
+				}
 
-                await database.SaveChangesAsync();
-                //return View(beers);  
+				// Update the GoesWellWith property
+				beer.GoesWellWith = string.Join(", ", mostSelectedCategories);
+			}
 
-        }
+			await database.SaveChangesAsync();
+		}
 
-        public async Task OnPostAsync(string findBeer, string[] beerType)
-        {
-            IQueryable<Beer> beers2Show = database.Beers;
+	public async Task OnPostAsync(string searchInput)
+	{
+		IQueryable<Beer> beers2Show = database.Beers;
 
-            if (!string.IsNullOrEmpty(findBeer))
-            {
-                beers2Show = beers2Show.Where(b => b.Name.Contains(findBeer));
-            }
-            else
-            {
-                Beer = await database.Beers.ToListAsync();
-            }
-            if (beerType != null && beerType.Any())
-            {
-                List<string> types = new List<string>();
-                if (beerType.Contains("Ale"))
-                {
-                    types.AddRange(new string[] { "Ale", "Brown Ale", "IPA", "Wheat Ale", "Belgian Ale", "Saison" });
-                }
-                if (beerType.Contains("Lager"))
-                {
-                    types.AddRange(new string[] { "Lager", "Kolsch", "STUFF HERE", "SORTER NÄR SOM NU" });
-                }
-                if (beerType.Contains("Stout"))
-                {
-                    types.AddRange(new string[] { "Stout", "Imperial Stout", "PUT THING HERE", "PUT IT HERE YES" });
-                }
-                beers2Show = beers2Show.Where(b => types.Contains(b.Type));
-            }
+		if (!string.IsNullOrEmpty(FindBeer))
+		{
+			beers2Show = beers2Show.Where(b => b.Name.Contains(FindBeer));
+		}
 
-            Beer = await beers2Show.ToListAsync();
-        }
+		if (!string.IsNullOrEmpty(searchInput))
+		{
+			beers2Show = beers2Show.Where(b => b.Name.Contains(searchInput) || b.EAN13 == searchInput);
+		}
 
-    }
+		if (BeerType != null && BeerType.Length > 0)
+		{
+			List<string> types = new List<string>();
+			if (BeerType.Contains("Ale"))
+			{
+				types.AddRange(new string[] { "Ale", "Brown Ale", "IPA", "Wheat Ale", "Belgian Ale", "Saison" });
+			}
+			if (BeerType.Contains("Lager"))
+			{
+				types.AddRange(new string[] { "Lager", "Kolsch", "STUFF HERE", "SORTER NÄR SOM NU" });
+			}
+			if (BeerType.Contains("Stout"))
+			{
+				types.AddRange(new string[] { "Stout", "Imperial Stout", "PUT THING HERE", "PUT IT HERE YES" });
+			}
+			beers2Show = beers2Show.Where(b => types.Contains(b.Type));
+		}
+
+		Beer = await beers2Show.ToListAsync();
+	}
+
+
+}
 }
