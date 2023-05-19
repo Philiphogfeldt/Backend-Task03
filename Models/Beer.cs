3f003d6f4b29c5dc72101873e6fb89df53d4eb60
TheAbsintheDuck <gabriel-carlsson@hotmail.com>
2023-05-19 14:27:29 +0200

    Trying my best

--- Models/Beer.cs
+++ Models/Beer.cs
@@ -8,14 +8,11 @@ namespace Backend_Task03.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
-
-		//public string Percentage { get; set; }
-		//[Required]
-        //[Range(0, 15, ErrorMessage = "Percentage must be between 0 and 15")]
         public double? Percentage { get; set; }
         public string Brewery { get; set; }
         public string Country { get; set; }
-        public string EAN13 { get; set; }
+		[RegularExpression("^[0-9]+$", ErrorMessage = "Only numbers are allowed.")]
+		public string EAN13 { get; set; }
         public string? GoesWellWith { get; set; } = "-";
         public double? Rating { get; set; }
         public List<Review>? Reviews { get; set; }
