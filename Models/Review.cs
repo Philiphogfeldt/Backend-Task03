abf8efb139b754e795bebfa7f7af0b581d471041
TheAbsintheDuck <gabriel-carlsson@hotmail.com>
2023-05-22 19:10:34 +0200

    Review update

--- Models/Review.cs
+++ Models/Review.cs
@@ -1,16 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Backend_Task03.Models;
+using System.ComponentModel.DataAnnotations;
 
-namespace Backend_Task03.Models
+public class Review
 {
-    public class Review
-    {
-        public int ID { get; set; }
-        public int Rating { get; set; }
-		[StringLength(180, ErrorMessage = "Comment must be at most 180 characters long.")]
-		public string? Comment { get; set; }
-        public DateTime Created { get; set; }
-        public Account Account { get; set; }
-        public Beer Beer { get; set; }
-        public List<FoodCategory> FoodCategories { get; set; } = new List<FoodCategory>();
-	}
+	public int ID { get; set; }
+	[Required]
+	public int Rating { get; set; }
+	[StringLength(180, ErrorMessage = "Comment must be at most 180 characters long.")]
+	public string? Comment { get; set; }
+	public DateTime Created { get; set; } = DateTime.Now;
+	public Account Account { get; set; }
+	public Beer Beer { get; set; }
+	public List<FoodCategory> FoodCategories { get; set; } = new List<FoodCategory>();
 }
