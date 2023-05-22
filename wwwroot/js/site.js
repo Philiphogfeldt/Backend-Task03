927c914e4e865b6d4163fcfc66e8176d8c4c0963
fredrikgreco <112250686+fredrikgreco@users.noreply.github.com>
2023-05-22 21:55:30 +0200

    mer inte klar CSS o java script

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,10 @@
 ﻿
+
+document.getElementById("@(item.Name)Checkbox").addEventListener("change", function () {
+    var label = document.querySelector(".beerFavoriteLabel");
+    if (this.checked) {
+        label.classList.add("checked");
+    } else {
+        label.classList.remove("checked");
+    }
+});
