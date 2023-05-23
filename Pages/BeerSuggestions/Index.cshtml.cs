b04bd7caf741145b91ec118fb13a2fb3703cbbac
Philip Högfeldt <112342813+Philiphogfeldt@users.noreply.github.com>
2023-05-23 23:41:52 +0200

    x

--- Pages/BeerSuggestions/Index.cshtml.cs
+++ Pages/BeerSuggestions/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
 
 namespace Backend_Task03.Pages.BeerSuggestions
 {
