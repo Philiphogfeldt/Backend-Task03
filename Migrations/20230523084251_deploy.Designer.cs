bd0326cd78d939c7a9b0931e5d1f150aef1525fd
Philip Högfeldt <112342813+Philiphogfeldt@users.noreply.github.com>
2023-05-23 10:54:15 +0200

    Azure

--- Migrations/20230523084251_deploy.Designer.cs
+++ Migrations/20230523084251_deploy.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend_Task03.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20230523081840_first")]
-    partial class first
+    [Migration("20230523084251_deploy")]
+    partial class deploy
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
