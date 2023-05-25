9bcccbbfc1ac4da5a4d92dc47bd89697ca0deeaf
Philip Högfeldt <112342813+Philiphogfeldt@users.noreply.github.com>
2023-05-25 18:34:05 +0200

    x

---
+++ Pages/Accounts/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Backend_Task03.Pages.Accounts
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
