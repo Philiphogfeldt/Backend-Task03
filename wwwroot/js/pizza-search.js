12de3717399736c29690b11a29e4cdde78355b1f
lindawendel <linda.m.wendel@gmail.com>
2023-05-29 22:32:19 +0200

    TryCatch in Pizzasearch

--- wwwroot/js/pizza-search.js
+++ wwwroot/js/pizza-search.js
@@ -4,9 +4,16 @@ const useRealAPI = true;
 
 async function fetchJSON(url, options) {
     if (useRealAPI) {
-        const response = await fetch(url, options);
-        const json = await response.json();
-        return json;
+        try {
+            const response = await fetch(url, options);
+            const json = await response.json();
+            return json;
+        }
+        catch
+        {
+            const message = document.querySelector('#message');
+            message.hidden = false;
+        }
     } else {
         // Sleep for one second to simulate a network delay.
         await new Promise((r) => setTimeout(r, 1000));
@@ -64,8 +71,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
 
                 // ska det ev vara: img.src = result.photo; eller img.src = result.photo.webformatURL; ? 
+           
                 img.src = result.webformatURL;
                 img.style.width = '250px';
+             
                 pizzaName.textContent = result.name;
 
                 const li = document.createElement('li');
@@ -75,7 +84,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 li.append(ingredientList);
 
                 resultList.append(li);
-            /*}*/
         }
     }
 
